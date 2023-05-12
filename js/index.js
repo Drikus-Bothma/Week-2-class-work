7259bf219e2ae774768e05cd5e5a4607027f5443
Drikus-Bothma <231155@virtualwindow.co.za>
2023-05-12 10:20:52 +0200

    Homework

---
+++ js/index.js
@@ -0,0 +1,42 @@
+let name = "Drikus";
+let surname = "Bothma";
+let age = 20;
+const subject = "Development";
+
+console.log("Hello, my name is " + name + " " + surname + ", I am " + age + " years old and I am studying " + subject);
+
+
+let numOne = 12
+let numTwo = 8
+let numThree = 36
+let numFour = 85
+let answer = 0
+
+answer = numOne + numTwo / numThree * numFour
+console.log(answer)
+
+
+let timmy = 10
+let susan = 25
+let value = 0
+
+timmyTax = timmy * 15 / 100
+susanTax = susan * 26 / 100
+value = timmyTax + susanTax
+
+console.log("The Tax Man will claim " + "R" + value + " from Timmy and Susan")
+
+
+let john = 59
+let tim = 89
+let alfie = 78
+let gwen = 30
+
+sum = john + tim + alfie + gwen
+value = sum / 400 * 100
+
+console.log("The students average is: " + value + "/100")
+
+value = value + 7
+
+console.log("The students updated average is: " + value + "/100")
